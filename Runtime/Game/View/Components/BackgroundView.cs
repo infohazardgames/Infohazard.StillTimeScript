e0b382f24d6203f3e0ff7624c5ccb44f9e443cc3
Val Miller <infohazard.games@gmail.com>
2026-08-10 00:14:10 -0700

    Remove dotween dependency

--- Runtime/Game/View/Components/BackgroundView.cs
+++ Runtime/Game/View/Components/BackgroundView.cs
@@ -1,18 +1,25 @@
-﻿using DG.Tweening;
+﻿using Infohazard.Core;
 using Infohazard.StillTimeScript.Core.Utility;
 using UnityEngine;
 
 namespace Infohazard.StillTimeScript.Game.View.Components {
     public class BackgroundView : GameViewComponent {
         public Camera _camera;
+        private PassiveTimer _timer;
+        private Color _sourceColor;
+        private Color _destColor;
 
         public void SetColor(StsColor color, float time) {
-            _camera.DOKill();
             Color unityColor = new(color.R, color.G, color.B, color.A);
             if (time == 0) {
                 _camera.backgroundColor = unityColor;
+                _sourceColor = _destColor = unityColor;
+                _timer.EndInterval();
             } else {
-                _camera.DOColor(unityColor, time);
+                _sourceColor = _camera.backgroundColor;
+                _destColor = unityColor;
+                _timer.Interval = time;
+                _timer.StartInterval();
             }
         }
 
@@ -20,6 +27,16 @@ namespace Infohazard.StillTimeScript.Game.View.Components {
             base.Clear();
 
             _camera.backgroundColor = Color.clear;
+            _sourceColor = _destColor = Color.clear;
+            _timer.EndInterval();
+        }
+
+        private void Update() {
+            if (!_timer.IsIntervalEnded) {
+                _camera.backgroundColor = Color.Lerp(_sourceColor, _destColor, _timer.RatioSinceIntervalStart);
+            } else if (_timer.DidIntervalEndThisFrame) {
+                _camera.backgroundColor = _destColor;
+            }
         }
     }
 }
