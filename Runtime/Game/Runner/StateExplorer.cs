78c1b97148aa77ece61379c1677c996e9009e48c
Val Miller <infohazard.games@gmail.com>
2026-08-11 21:55:07 -0700

    Workaround for countdown exploration bug

--- Runtime/Game/Runner/StateExplorer.cs
+++ Runtime/Game/Runner/StateExplorer.cs
@@ -4,12 +4,20 @@ using System.Linq;
 using Infohazard.StillTimeScript.Core.Nodes;
 using Infohazard.StillTimeScript.Core.Resource;
 using Infohazard.StillTimeScript.Core.State;
+using Infohazard.StillTimeScript.Core.Utility;
 using UnityEngine;
 
 namespace Infohazard.StillTimeScript.Game.Runner {
     public class StateExplorer : MonoBehaviour {
         public StateAdvancer _stateAdvancer;
         public string _exploredScopeName;
+        public string[] _ignoredVariables;
+
+        private HashSet<string> _ignoredVariablesSet;
+
+        private void Awake() {
+            _ignoredVariablesSet = new HashSet<string>(_ignoredVariables);
+        }
 
         public bool ExploreBranchForNewContent(
             GameGraph graph,
@@ -33,6 +41,7 @@ namespace Infohazard.StillTimeScript.Game.Runner {
 
             try {
                 stack.Add(state);
+                VariablesComponent stateVariables = state.GetOrCreate<VariablesComponent>();
 
                 foreach (INode possibleNext in currentNode.GetPossibleNextNodes(state)) {
                     if (possibleNext is null or ResetScopeNode) continue;
@@ -40,9 +49,9 @@ namespace Infohazard.StillTimeScript.Game.Runner {
                     StateContainer previousStateAtNode =
                         stack.FindLast(s => s.GetOrCreate<CurrentNodeComponent>().CurrentNode == possibleNext);
 
-                    if (previousStateAtNode != null &&
-                        previousStateAtNode.GetOrCreate<VariablesComponent>().Variables
-                                           .SequenceEqual(state.GetOrCreate<VariablesComponent>().Variables)) {
+                    VariablesComponent previousVariables = previousStateAtNode?.GetOrCreate<VariablesComponent>();
+
+                    if (previousVariables != null && VariablesAreEqual(stateVariables, previousVariables)) {
                         continue;
                     }
 
@@ -59,6 +68,25 @@ namespace Infohazard.StillTimeScript.Game.Runner {
             }
 
             return false;
+        }
+
+        private bool VariablesAreEqual(VariablesComponent var1, VariablesComponent var2) {
+            Dictionary<Variable, StsValue>.KeyCollection keys1 = var1.Variables.Keys;
+            Dictionary<Variable, StsValue>.KeyCollection keys2 = var2.Variables.Keys;
+
+            HashSet<Variable> variables = new(keys1.Concat(keys2));
+
+            foreach (Variable variable in variables) {
+                if (_ignoredVariablesSet.Contains(variable.Identifier)) continue;
+
+                if (!var1.Variables.TryGetValue(variable, out StsValue value1) ||
+                    !var2.Variables.TryGetValue(variable, out StsValue value2) ||
+                    !value1.Equals(value2)) {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
